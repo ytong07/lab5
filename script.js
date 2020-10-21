1727d827013bdc68e29a3ed7b1fdb04d0c9856a0
Yun Tong <tongyd@bc.edu>
2020-10-22 01:27:57 +0800

    final

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 // CHART INIT ------------------------------
-// create svg with margin convention //
-let type = document.querySelector('#group-by').value;
-
-//console.log("Selected Node: ", type)
-
+// console.log("Selected Node: ", type)
 const margin = ({top: 20, right: 20, bottom: 20, left: 50});
 const width = 630 - margin.left - margin.right;
-const height = 600 - margin.top - margin.bottom;
+const height = 500 - margin.top - margin.bottom;
 const svg = d3.select(".chart")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -17,27 +13,24 @@ const svg = d3.select(".chart")
 // create scales WITHOUT domains
 let xScale = d3
     .scaleBand()
-    //.domain(data.map(d=>d.company))
-    .rangeRound([0, height])
+    .rangeRound([0, width])
     .paddingInner(0.1);
 let yScale = d3
     .scaleLinear()
-    //.domain(d3.extent(data, d=>d.stores))
-    .range([width,0]);
+    .range([height,0]);
 ///////console.log("X DOMAIN INIT: ", xScale.domain)
 // create axes and axis title containers
 let xAxis = d3.axisBottom()
-		.scale(xScale);
+              .scale(xScale);
 let yAxis = d3.axisLeft()
-        .scale(yScale);
+              .scale(yScale);
 
 svg.append('g')//x axis
     .attr('class', 'axis x-axis')
-    .attr("transform", `translate(0, ${width})`)
-    .call(xAxis);      
+    .attr("transform", `translate(0, ${height})`);      
 svg.append('g')//y axis
     .attr('class', 'axis y-axis')
-    .call(yAxis);
+    .attr("transform", "translate(" + 0 + ", 0)");
 svg.append("text")//y axis label
   .attr("class", "y-axis-title")
   .attr('x', 0)
@@ -45,66 +38,101 @@ svg.append("text")//y axis label
   .attr('font-size','12px')
   .style("text-anchor", "front");
 
-// (Later) Define update parameters: measure type, sorting direction!!!!!!!!!!!!!!!
+// CHART UPDATES ---------------------------
+let data = null;
+d3.csv("coffee-house-chains.csv", d=>{ 
+        return {...d, revenue: +d.revenue, stores: +d.stores};})
+  .then(rev => {data = rev;
+                update();});
+
+  //event listener for dropdown
+  dropdown = document.querySelector('#group-by');
+  dropdown.addEventListener('change', function(){ return update();});
+
+
+  //Handling the sorting direction change
+  let sortChart = true;
+  d3.select('.button').on('click', () => {sortChart = !sortChart; update();});
+
 
 // CHART UPDATE FUNCTION -------------------
-function update(data,type){
-	// update domains
-    xScale.domain(data.map(d=>d.company));
-    yScale.domain([0, d3.max(data, d=>d[type])]);//?????
-    console.log("X DOMAIN UPDATE: ", xScale.domain)
-    const bars = svg.selectAll('.bar')
-                    .data(data);
-    // update bars
-    // Implement the enter-update-exit sequence
-    //Make sure to add a data key function
-    //(Hint: Use the company name as a key
-    bars.selectAll('rect')
-        .data(data)
-        .enter()
-        .append('rect')
-        .attr('x', (d,i)=>(i * 69))
-        .attr('width', xScale.bandwidth())
-        .attr('y', d=>yScale(d[type]))
-        .attr('height', d=>height-yScale(d[type]))
-        .attr("fill", "blue")
-        .attr('class', 'bar');
-    bars.exit().remove();
-    bars.transition()
-        .duration(1000)
-        .attr('y', d=>yScale(d[type]))
-        .attr('height', d=>height-yScale(d[type]))
+function update(){
+    // create svg with margin convention //
+    let type = document.querySelector('#group-by').value;
 
-    // update axes and axis title
-    svg.select('.axis x-axis')
-        //.attr("transform", `translate(0, ${width})`)
-        .call(xAxis);
-    svg.select('.axis y-axis')
-        .call(yAxis);
-    if(type == 'stores'){
-        svg.select('.y-axis-title')
-            .text('Stores');
+    console.log("update", data)
+    // update domains
+    // Sort data
+  data.sort((a, b) => type == "stores"
+    ? sortChart
+      ? d3.descending(a.stores, b.stores)
+      : d3.ascending(a.stores, b.stores)
+    : sortChart
+    ? d3.descending(a.revenue, b.revenue)
+    : d3.ascending(a.revenue, b.revenue));
+  
+  xScale.domain(data.map(d => d.company));
+  if (type == "stores") {
+      let storesMax = d3.max(data, d => d.stores);
+      yScale = d3
+        .scaleLinear()
+        .domain([0, storesMax])
+        .range([height, 0]);
     }
-    else{
-        svg.select('.y-axis-title')
-            .text('Billion USD');
+  else {
+      let revenueMax = d3.max(data, d => d.revenue);
+      yScale = d3
+        .scaleLinear()
+        .domain([0, revenueMax])
+        .range([height, 0]);
     }
-        
-}
-
-// CHART UPDATES ---------------------------
-d3.csv('coffee-house-chains.csv', d3.autoType).then(data=>{
-    //console.log('type: ', typeof type,type);
-    data = data.sort((a, b) => d3.descending(a[type], b[type]));
-    //console.log('coffee house chains', data);
-    update(data,type); // simply call the update function with the supplied data
+  
+  //console.log("X DOMAIN UPDATE: ", xScale.domain)
+  // update bars
+  // Implement the enter-update-exit sequence
+  let bars = svg.selectAll('rect')
+                .data(data, d=>{return d.company})
     
-    // (Later) Handling the type change
-    dropdown = document.querySelector('#group-by');
-    dropdown.addEventListener('change', function(){ type = dropdown.value});
+  bars.enter()
+      .append('rect')
+      .attr('class', 'bar')   
+      .merge(bars)
+      .transition()
+      .duration(300)
+      .attr("x", d => xScale(d.company))
+      .transition()
+      .duration(300)
+      .attr("y", d => yScale(type == "stores" ? d.stores : d.revenue))
+      .attr("width", xScale.bandwidth())
+      .attr("height", d => height - yScale(type == "stores" ? d.stores : d.revenue));
+  bars.exit()
+      .remove();
+  svg.exit().remove();
 
-
-    // (Later) Handling the sorting direction change
-    let sortChart = () => {data.reverse();};
-    window.onload = () => {document.getElementById('group-by').onclick = sortChart;}
-});
+  // update axes and axis title
+  xAxis.scale(xScale);
+  yAxis.scale(yScale);
+  
+  svg.select('.x-axis')
+    .transition()
+    .duration(300)
+    .call(xAxis);
+  
+  svg.select(".y-axis")
+    .transition()
+    .duration(300)
+    .style('opacity', 0)
+    .transition()
+    .duration(300)
+    .style('opacity', 1)
+    .call(yAxis);
+    
+  if(type == 'stores'){
+      svg.select('.y-axis-title')
+          .text('Stores');
+  }
+  else{
+      svg.select('.y-axis-title')
+          .text('Billion USD');
+  }      
+}
